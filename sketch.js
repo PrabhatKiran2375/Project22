4e3b0283688484c73818279ffc2a1e5fe3b8022d
PrabhatKiran2375 <82816747+PrabhatKiran2375@users.noreply.github.com>
2021-05-17 21:15:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,17 +31,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
- // create a ground
-    ground= Bodies.rectangle(width/2,650,width,10 ,{isStatic:true});
-	World.add(world,ground);
 
 
 
